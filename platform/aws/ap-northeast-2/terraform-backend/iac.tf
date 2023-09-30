de7f6db84b5219f77fbac78c2484dddc06c01cdc
jmj941022 <jch941022@naver.com>
2023-09-30 19:38:28 +0900

    first commit

---
+++ platform/aws/ap-northeast-2/terraform-backend/iac.tf
@@ -0,0 +1,3 @@
+module "terraform-backend" {
+  source = "git@github.com:jominjun94/platform-repository.git//aws/ap-northeast-2/terraform-backend?ref=main"
+}
---
+++ platform/aws/ap-northeast-2/terraform-codes/iac.tf
@@ -0,0 +1,3 @@
+module "terraform-codes" {
+  source = "git@github.com:jominjun94/platform-repository.git//aws/ap-northeast-2/terraform-codes?ref=main"
+}
