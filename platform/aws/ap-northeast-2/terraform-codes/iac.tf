50660b6806dff1770e5ad5afe74966177225d3aa
jmj941022 <jch941022@naver.com>
2023-09-30 19:39:35 +0900

    git commit

--- platform/aws/ap-northeast-2/terraform-codes/iac.tf
+++ platform/aws/ap-northeast-2/terraform-codes/iac.tf
@@ -1,3 +1,3 @@
 module "terraform-codes" {
-  source = "git@github.com:jominjun94/platform-repository.git//aws/ap-northeast-2/terraform-codes?ref=main"
+  source = "git@github.com:jominjun94/terraform-repository.git//aws/ap-northeast-2/terraform-codes?ref=main"
 }
