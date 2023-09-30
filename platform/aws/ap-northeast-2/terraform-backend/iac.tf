eb8ccccece4161fb830249f2dac9d16ea2c8aac1
jmj941022 <jch941022@naver.com>
2023-09-30 19:40:24 +0900

    git commit

--- platform/aws/ap-northeast-2/terraform-backend/iac.tf
+++ platform/aws/ap-northeast-2/terraform-backend/iac.tf
@@ -1,3 +1,3 @@
 module "terraform-backend" {
-  source = "git@github.com:jominjun94/platform-repository.git//aws/ap-northeast-2/terraform-backend?ref=main"
+  source = "git@github.com:jominjun94/terraform-repository.git//aws/ap-northeast-2/terraform-backend?ref=main"
 }
